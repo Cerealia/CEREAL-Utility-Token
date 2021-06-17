aed093d22091094f555e474e6fe6f79c75821b3a
Jaime Delgado <jfdelgad@gmail.com>
2021-06-17 12:06:13 +0200

    remove unused variable, limit compiler version to 0.8.x

--- contracts/cereal.sol
+++ contracts/cereal.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: NONE
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Cereal is ERC20 {
-    address owner;
-
     constructor() ERC20("Cereal", "CRL") {
-        owner = msg.sender;
-        _mint(owner, 10000000 * (10**18));
+        _mint(msg.sender, 10000000 * (10**18));
     }
 }
